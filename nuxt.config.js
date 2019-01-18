c3606ed7df31d74e60e54be66dd3f5cf9c8a6214
lsrawat <lakshit.rawat@techolution.com>
2019-01-18 18:07:35 +0530

    added desc for seo and canonical link tag

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,14 +3,16 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: 'portfolio',
+    title: 'Todo App',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Nuxt.js project' }
+      { hid: 'description', name: 'description', content: 'Practice project for nuxt tutorial' },
+      { }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      {rel: 'canonical', href:'http://www.sitename.com'}
     ]
   },
   /*
