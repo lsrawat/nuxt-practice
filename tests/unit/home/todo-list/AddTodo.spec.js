93d04d6266424fb762e0eeec865838b6e41bf5ff
lsrawat <lakshit.rawat@techolution.com>
2019-01-18 21:56:37 +0530

    added test cases for new functionality

--- tests/unit/home/todo-list/AddTodo.spec.js
+++ tests/unit/home/todo-list/AddTodo.spec.js
@@ -1,32 +1,48 @@
+jest.mock('../../../../services/store-service');
+
 import { createLocalVue, mount } from '@vue/test-utils';
 import AddTodo from '@/components/home/AddTodo';
 import Vuex from 'vuex';
 import storeService from '../../../../services/store-service';
 
 const localVue = createLocalVue();
-
 localVue.use(Vuex);
-describe('TodoList.vue', () => {
-  const wrapper = mount(AddTodo);
+describe('AddTodo.vue', () => {
+  let store;
+  let getters;
+  let wrapper;
   beforeEach(() => {
-    
+    getters = {
+      currentId: () => 0
+    };
+    store = new Vuex.Store({
+      getters
+    });
+    wrapper = mount(AddTodo, { store, localVue });
   })
-  it('should contain a button and an input', () => {
+  it('should contain a button and an input', (done) => {
     expect(wrapper.contains("button")).toBe(true);
     expect(wrapper.contains("input")).toBe(true);
+    done();
   })
-  it('should emit a todo object on button click', () => {
+
+  it('should emit a todo object on button click and increment id.', (done) => {
     const button = wrapper.find('button');
     button.trigger('click');
+    expect(storeService.incrementId).toBeCalled();
+    expect(wrapper.vm.currentId).toEqual(wrapper.vm.todo.id);
     wrapper.vm.$emit('add-todo', wrapper.vm.todo);
     expect(wrapper.emitted()['add-todo'][0]).toEqual([wrapper.vm.todo]);
+    done();
   })
-  it('should have default todo values', () => {
+
+  it('should have default todo values', (done) => {
     expect(wrapper.vm.todo).toEqual({
       id: 0,
       title: "",
       userId: 0,
       completed: false
     });
+    done();
   })
 })
--- tests/unit/home/todo-list/TodoList.spec.js
+++ tests/unit/home/todo-list/TodoList.spec.js
@@ -1,46 +1,60 @@
-import { shallowMount, createLocalVue } from '@vue/test-utils';
+jest.mock('../../../../services/store-service');
+// jest.mock('../../../../mixins/todoFilterMixin');
+
+import { mount, createLocalVue } from '@vue/test-utils';
 import TodoList from '@/components/home/todo-list/TodoList';
 import Vuex from 'vuex';
+import storeService from '../../../../services/store-service';
+import todoFilterMixin from '../../../../mixins/todoFilterMixin';
+
 
 const localVue = createLocalVue();
 
 localVue.use(Vuex);
-describe('getters in todolist.vue', () => {
 
-})
 describe('TodoList.vue', () => {
 
-  let store, wrapper;
+  let store, wrapper, getters, pending, completed;
   beforeEach(() => {
-    store = {
-      dispatch: jest.fn()
+    getters = {
+      todoList: () => [{ completed: true }, { completed: false }, { completed: false }]
     };
-    wrapper = shallowMount(TodoList, {
-      propsData: {
-        todos: [{ completed: true }, { completed: false }, { completed: false }]
-      },
-      store,
-      localVue
-    }).vm
+    store = new Vuex.Store({
+      getters
+    });
+    wrapper = mount(TodoList, {
+      store, localVue
+    })
+    // console.log(wrapper.vm.listFilterByStatus);
   })
-  it('current id should be set', (done) => {
-    expect(store.dispatch).toBeCalledWith('setCurrentId', 3);
-    expect(wrapper.pending.length).toBe(2);
-    expect(wrapper.completed.length).toBe(1);
-    done();
-  })
+  // it('current id should be set and list should be filtered into pending and completed.', (done) => {
+  //   // console.log(wrapper.vm.todoList.length);
+  //   // expect(storeService.setCurrentId).toBeCalledWith(3);
+  //   // wrapper.vm.listFilterByStatus = jest.fn();
+  //   wrapper.
+  //   expect(wrapper.vm.listFilterByStatus).toBeCalled();
+  //   // expect(wrapper.vm.listFilterByStatus).toBeCalledWith(wrapper.vm.todoList, false);
+  //   // expect(wrapper.vm.pending.length).toBe(2);
+  //   // expect(wrapper.vm.completed.length).toBe(1);
+  //   done();
+  // })
   describe('methods', () => {
     it('addTodoToList', (done) => {
-      wrapper.addTodoToList({ completed: false });
-      expect(wrapper.pending.length).toBe(3);
+      wrapper.vm.addTodoToList({ completed: false });
+      expect(storeService.setTodos).toBeCalledWith(wrapper.vm.todoList);
+      setTimeout(() => {
+        expect(wrapper.vm.pending.length).toBe(3);
+      }, 0);
       done();
     });
 
     it('onMarkAsComplete', (done) => {
-      wrapper.onMarkAsComplete({ todo: { completed: false }, index: 1 });
-      expect(wrapper.pending.length).toBe(1);
-      expect(wrapper.completed.length).toBe(2);
-      done()
+      wrapper.vm.onMarkAsComplete(wrapper.vm.todoList[1]);
+      setTimeout(() => {
+        expect(wrapper.vm.pending.length).toBe(1);
+        expect(wrapper.vm.completed.length).toBe(2);
+      })
+      done();
     })
   })
 
