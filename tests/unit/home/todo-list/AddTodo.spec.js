0f770343e84bf4bcf4c288df19e60ab5818ca341
lsrawat <lakshit.rawat@techolution.com>
2019-01-18 19:20:19 +0530

    test case changes'

--- tests/unit/home/todo-list/AddTodo.spec.js
+++ tests/unit/home/todo-list/AddTodo.spec.js
@@ -1,29 +1,21 @@
 import { createLocalVue, mount } from '@vue/test-utils';
 import AddTodo from '@/components/home/AddTodo';
 import Vuex from 'vuex';
+import storeService from '../../../../services/store-service';
 
 const localVue = createLocalVue();
 
 localVue.use(Vuex);
 describe('TodoList.vue', () => {
-  let getters;
-  let store;
   const wrapper = mount(AddTodo);
   beforeEach(() => {
-    getters = {
-      currentId: () => 0
-    }
-    store = new Vuex.Store({
-      getters
-    })
+    
   })
   it('should contain a button and an input', () => {
     expect(wrapper.contains("button")).toBe(true);
     expect(wrapper.contains("input")).toBe(true);
   })
   it('should emit a todo object on button click', () => {
-    
-    const input = wrapper.find('input');
     const button = wrapper.find('button');
     button.trigger('click');
     wrapper.vm.$emit('add-todo', wrapper.vm.todo);
